5bf743308c8a66a2af6a7b7374ba360509addb6b
Floris Porro <floris@streammyevent.com>
2022-11-04 18:06:31 +0100

    Stats displays

--- src/lib/components/display/singlevaluecard.svelte
+++ src/lib/components/display/singlevaluecard.svelte
@@ -3,7 +3,7 @@
 	export let value = '';
 </script>
 
-<div class="bg-white rounded-2xl shadow-lg px-8 py-4 flex flex-col justify-center">
+<div class="bg-white rounded-2xl shadow-lg px-8 py-4 flex flex-col justify-center text-center">
 	<h4 class="text-2xl font-bold">{name}</h4>
 	<p class="text-4xl font-bold"><slot /></p>
 </div>
--- src/lib/components/statsdisplay.svelte
+++ src/lib/components/statsdisplay.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-	import type { Stats } from '../interface';
+	import type { Flight } from '../interface';
 
 	import { getStatistics } from '../stats';
+	import type { Stats } from '../stats';
 
 	import SingleValueCard from './display/singlevaluecard.svelte';
 	import TimeDisplay from './display/timedisplay.svelte';
+	import KeyValue from './display/keyvalue.svelte';
 
-	export let data: Stats[];
+	export let data: Flight[];
 
-	export let statistics: object = {};
+	export let statistics: Stats;
 
 	$: statistics = getStatistics(data);
 	$: console.log(statistics);
@@ -19,25 +21,58 @@
 		<h2>Hallo, {statistics.pilot}</h2>
 		<h3>Dit zijn jouw totalen:</h3>
 	</div>
-	<div class="grid grid-cols-3 gap-6">
+	<div class="grid grid-cols-2 gap-6 mb-12">
 		<SingleValueCard name="Totaal starts">{data.length}</SingleValueCard>
+		<SingleValueCard name="Totaal tijd"><TimeDisplay value={statistics.picTime} /></SingleValueCard>
+	</div>
+	<div class="grid grid-cols-2 gap-6 mb-12">
 		<SingleValueCard name="Totaal DBO starts">{statistics.dboFlights.length}</SingleValueCard>
-		<SingleValueCard name="Totaal PIC starts">{statistics.picFlights.length}</SingleValueCard>
-		<SingleValueCard name="Totaal starts als PAX">{statistics.paxFlights.length}</SingleValueCard>
 		<SingleValueCard name="DBO tijd"><TimeDisplay value={statistics.dboTime} /></SingleValueCard>
+	</div>
+	<div class="grid grid-cols-2 gap-6 mb-12">
+		<SingleValueCard name="Totaal PIC starts">{statistics.picFlights.length}</SingleValueCard>
 		<SingleValueCard name="PIC tijd"><TimeDisplay value={statistics.picTime} /></SingleValueCard>
+	</div>
+	<div class="grid grid-cols-4 gap-6 mb-12">
+		<SingleValueCard name="Examen vluchten">{statistics.examFlights.length}</SingleValueCard>
+		<SingleValueCard name="Examen gehaald">{statistics.hasLicense ? '✅' : '❌'}</SingleValueCard>
+		<SingleValueCard name="Totaal starts als PAX">{statistics.paxFlights.length}</SingleValueCard>
 		<SingleValueCard name="PAX tijd"><TimeDisplay value={statistics.paxTime} /></SingleValueCard>
 		<SingleValueCard name="Overlandjes">{statistics.xcountryFlights.length}</SingleValueCard>
-		<SingleValueCard name="Overland pogingen (<33 min)"
-			>{statistics.xcountryattemptFlights.length}</SingleValueCard
+		<SingleValueCard name="Overland pogingen"
+			>{statistics.xcountryattemptFlights.length +
+				statistics.xcountryFlights.length}</SingleValueCard
 		>
 		<SingleValueCard name="Laatste overland">{statistics.xcountryFlights[0].datum}</SingleValueCard>
 		<SingleValueCard name="Buitenlandingen">{statistics.outlandings.length}</SingleValueCard>
 	</div>
+	<h3>Dit zijn je starts per jaar:</h3>
+	<KeyValue data={statistics.flightsByYear} />
+	<h3>Dit zijn je starts per startmiddel:</h3>
 	<h3>Dit zijn je starts per vliegtuigtype:</h3>
-	<h3>Dit zijn je starts per jaar:</h3>
-	<h3>Dit zijn je starts per jaar:</h3>
+	<h3>Dit zijn je starts per vliegveld:</h3>
+	<h3>Dit zijn je statistieken sinds je laatste examen:</h3>
+	<p>Let op: het systeem weet niet of je geslaagd bent...</p>
 	<h3>Dit zijn je bevoegdheden:</h3>
+	<h3>Dit zijn je achievements:</h3>
+	<ul>
+		<li>3-uurs vlucht</li>
+		<li>5-uurs vlucht</li>
+		<li>6-uurs en 57 minuten vlucht</li>
+		<li>7-uurs vlucht</li>
+		<li>Houten Reet - 3-uur of langer in oud hout</li>
+		<li>Plastic Reet - 5-uur of langer in 21 of 23</li>
+		<li>Kunststof Reet - 8-uur of langer in een prestatiekist</li>
+		<li>St-akker - Buitenlanding binnen 60 minuten</li>
+		<li>Overland met een Beeke</li>
+		<li>Enkeltje Bel - Buitenlanding op Terlet</li>
+		<li>Drentse gastvrijheid - Buitenlanding op Salland</li>
+		<li>Net geen Duitsland - Buitenlanding op Venlo</li>
+		<li>Buitenlanding op Flevo</li>
+		<li>Rondje Rondweg - Gevlogen op Jena</li>
+		<li>Gevlogen op Stendal</li>
+		<li>Ervaren kampganger - Voor 2022 gevlogen op Stendal</li>
+	</ul>
 </div>
 
 <style lang="postcss">
