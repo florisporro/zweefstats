d3283dd1e5a49cca0bfe0ba46380184b8f2000f8
Floris Porro <floris@streammyevent.com>
2023-04-12 08:12:12 -0700

    fix:Waarschuwing toegevoegd over CSV export probleem

--- src/routes/load/csv/+page.svelte
+++ src/routes/load/csv/+page.svelte
@@ -29,7 +29,29 @@
 	}
 </script>
 
-<p class="text-white text-center">
+<div class="alert alert-error shadow-lg mb-12">
+	<div>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="stroke-current flex-shrink-0 h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+			/></svg
+		>
+		<span
+			>Er gaat op dit moment iets fout bij het exporteren van CSV files uit de Zweef app. De
+			statistieken zullen onvolledig zijn, het is nu beter om de directe import functie te
+			gebruiken.</span
+		>
+	</div>
+</div>
+
+<p class="text-center">
 	Volg de volgende stappen om je CSV bestand te downloaden uit de Zweef app:
 </p>
 
